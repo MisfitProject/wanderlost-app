3116e325becbe33b8351be34824ba92f2b7a4f32
Wanderlost AI <wanderlost@app.dev>
2026-04-01 23:46:32 +0200

    v63: NUKED EVERYTHING. Single-file app. index.html + sw.js = 236 lines total. All pages, all buttons, all logic.

--- sw.js
+++ sw.js
@@ -1,22 +1,4 @@
-const CACHE = 'wanderlost-v62';
-const FILES = ['./', './index.html', './app.js', './style.css', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+const C='wl-v63';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json'])));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
